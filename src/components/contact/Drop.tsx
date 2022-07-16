433ad1d98d7d3447dfdcd14beed78a43e65ab235
abiraltamang <abiral.tamang816@gmail.com>
2022-07-16 13:33:21 +0545

    contact us

--- src/components/contact/Drop.tsx
+++ src/components/contact/Drop.tsx
@@ -5,7 +5,7 @@ export default function Drop() {
         <div className='my-4 sm:my-8 md:my-12 lg:my-16 space-y-3 sm:space-y-5  md:space-y-10'>
             <h2 className="text-2xl sm:text-3xl lg:text-heading   font-Jost font-bold  lg:leading-[51px] text-themetext text-center">Drop Us a Line</h2>
 
-            <p className=' mx-4 xs:mx-14 sm:mx-24 md:mx-36 lg:mx-72 xl:mx-96 font-Inter font-medium text-themetext text-center  text-sm xs:text-base'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p >
+            <p className=' mx-4  xs:mx-14 sm:mx-24 md:mx-36 lg:mx-72 xl:mx-96 font-Inter font-medium text-themetext text-center  text-sm xs:text-base'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p >
             <div className='flex flex-col items-center justify-center'>
                 <div className='grid grid-cols-1 xs:grid-cols-2 xs:gap-x-4 sm:gap-x-[50px] gap-y-5 xs:gap-y-[32px] '>
                     <div>
